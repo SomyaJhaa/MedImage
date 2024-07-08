fb004e14cfe11a04a4df4b1a6cabb3e5ec74859f
Somya Jha <131262352+SomyaJhaa@users.noreply.github.com>
2024-07-09 01:55:08 +0530

    Update firbase_config.js

--- front  client/src/components/firbase_config.js
+++ front  client/src/components/firbase_config.js
@@ -8,13 +8,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBubN_2MxEFI7MNKo-yf4zgYl5O9EZo-cY",
-  authDomain: "first-cac5b.firebaseapp.com",
-  projectId: "first-cac5b",
-  storageBucket: "first-cac5b.appspot.com",
-  messagingSenderId: "416471197747",
-  appId: "1:416471197747:web:8a148f8ab1812e205edb46",
-  measurementId: "G-8P22QWEDEE"
+  
 };
 
 // Initialize Firebase
